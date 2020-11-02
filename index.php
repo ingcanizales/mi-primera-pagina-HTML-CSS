d3f7d3daf2f58f237092bcff46999c5c38fbcf55
jimmy de jesus cañizales <ingcanizales2793@gmail.com>
2020-11-02 08:03:05 -0500

    se agrega pagina web con imagenes

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>
+        HISTORIA DE COLON
+
+    </title>
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+    
+<!-- esto es un titulo-->
+<H1 align="center"> CRISTOBAL COLON (Cristóforo Colombo; Génova), 1451 - Valladolid, 1506)
+
+</H1>
+
+<hr color="blue" size=3> 
+<p> 
+ Descubridor de América. El origen de este navegante, probablemente italiano, está envuelto en el misterio por obra de él mismo y de su primer biógrafo, su hijo Hernando Colón. Parece ser que Cristóbal Colón empezó como artesano y comerciante modesto y que tomó contacto con el mar a través de la navegación de cabotaje con fines mercantiles.
+ </p>
+ <p> 
+ <img style="float:right; margin:10px;" src="images/cristobal.jpg"/>De unos y otros llegó a Cristóbal Colón la idea de que, siendo la Tierra esférica, la costa oriental de Asia podría alcanzarse fácilmente navegando hacia el oeste. Una serie de cálculos erróneos le habían hecho subestimar el perímetro terrestre y le llevaron a suponer, en consecuencia, que Japón se hallaba a 2.400 millas marinas de Canarias, distancia que, en realidad, es la que separa las Antillas del archipiélago canario.
+</p>
+<h2>
+VIAJES A AMERICA
+</h2>
+<P>
+Colón ofreció su proyecto al rey Juan II de Portugal, quien lo sometió al examen de un comité de expertos. Aunque terminó rechazando la propuesta, el monarca portugués puso previamente como condición que no se zarpase desde las Canarias, pues, en caso de que el viaje tuviera éxito, la Corona de Castilla podría reclamar las tierras conquistadas en virtud del Tratado de Alcaçobas. Colón encontró demasiado arriesgado partir de Madeira (sólo confiaba en los cálculos que había trazado desde las Canarias) y probó suerte en España con el duque de Medina Sidonia y con los Reyes Católicos, que rechazaron su propuesta por considerarla inviable y por las desmedidas pretensiones de Colón.
+
+</P>
+<center><img style="margin:10px;" width="400" height"650" src="images/colon_descubrimiento_america.jpg" /></center>
+<!--<img id= "cristobal"src="images/colon_descubrimiento_america.jpg" width="200" height"325">-->
+</body>
+
+</html>
